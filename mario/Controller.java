07c1ade873d0e24bf0c07dfdb239a73286f71879
emil-KEA <emil33v4@stud.kea.dk>
2022-04-25 12:21:58 +0200

    Changes by Mike

--- mario/Controller.java
+++ mario/Controller.java
@@ -5,8 +5,10 @@ public class Controller {
     public static void main(String[] args) {
         Ui ui = new Ui();
         ui.commandHelp();
-        while (ui.running) {
-            ui.userInput();
+        POS pointOfSales = new POS();
+        pointOfSales.commands();
+       // while (true) { //make private + add getter
+         //   ui.userInput();
         }
     }
 }
