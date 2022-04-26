d8815562ca96e655c59643753cb487715d3f1444
emil-KEA <emil33v4@stud.kea.dk>
2022-04-26 17:23:26 +0200

    næsten færdigt

--- mario/Controller.java
+++ mario/Controller.java
@@ -1,24 +1,18 @@
 package mario;
 
 public class Controller {
-   private boolean running = true;
-
-   public boolean getRunning() {
-   return this.running;
-   }
-
-    public void setRunning(boolean running){
-    this.running = running;
-   }
-
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        ui.commandHelp();
-        POS pointOfSales = new POS();
-
-        pointOfSales.commands();
-       // while (true) { //make private + add getter
-         //   ui.userInput();
-        }
+    private boolean running = true;
+    public boolean getRunning() {
+        return this.running;
     }
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+    public static void main(String[] args) {
+
+        POS pointOfSales = new POS();
+        pointOfSales.ui.commandHelp();
+        pointOfSales.commands();
+    }
+}
 
--- mario/POS.java
+++ mario/POS.java
@@ -6,9 +6,9 @@ public class POS {
   Ui ui = new Ui();
   Controller control = new Controller();
   Menu theMenu = new Menu();
-  ArrayList<Pizza> ordersQueue = new ArrayList<>();
-  ArrayList<Pizza> currentPizzaOrder = new ArrayList<>();
-  ArrayList<ArrayList<Pizza>> currentOrder = new ArrayList<>();
+  private ArrayList<Pizza> ordersQueue = new ArrayList<>();
+  private ArrayList<Pizza> currentPizzaOrder = new ArrayList<>();
+  private ArrayList<ArrayList<Pizza>> currentOrder = new ArrayList<>();
   POS.FinishedOrders finished = new POS.FinishedOrders();
 
   public void commands() {
@@ -28,7 +28,7 @@ public class POS {
   }
 
 
-  public void addToQueue() {
+  private void addToQueue() {
     boolean endOrder = false;
     System.out.println("Enter pizza ID: ");
     while (!endOrder) {
@@ -47,13 +47,13 @@ public class POS {
     }
   }
 
-  public void printQueue() {
+  private void printQueue() {
     System.out.println("Order queue: ");
     for (int i = 0; i < ordersQueue.size(); i++)
       System.out.println(ordersQueue.get(i));
   }
 
-  public void printFinishedOrders() {
+  private void printFinishedOrders() {
     int totalPrice = 0;
     System.out.println("Finished orders: ");
     for (int i = 0; i < finished.finishedOrderList.size(); i++) {
@@ -65,7 +65,7 @@ public class POS {
     System.out.println("Total income: " + totalPrice + "\n");
   }
 
-  public void endOrder() {
+  private void endOrder() {
     if (ordersQueue.size() != 0) {
       finished.addPizza(ordersQueue.get(0));
       ordersQueue.remove(0);
@@ -76,29 +76,29 @@ public class POS {
   public void endOfDayDetails() {
   }
 
-  public void addPizzaToOrderQueue(Pizza pizza) {
+  private void addPizzaToOrderQueue(Pizza pizza) {
     ordersQueue.add(pizza);
     currentPizzaOrder.add(pizza);
   }
 
-  public void currentOrder(ArrayList<Pizza> a) {
+  private void currentOrder(ArrayList<Pizza> a) {
     for(int i = 0; i < a.size(); i++) {
       a.get(i).printPizza();}
 
     currentOrder.clear();
   }
 
-  public void printCurrentOrderPrice() {
+  private void printCurrentOrderPrice() {
     int total = 0;
     for (int i = 0; i < currentPizzaOrder.size(); i++)
       total += currentPizzaOrder.get(i).getPrice();
     System.out.println("\nTotal Price: " + total);
   }
 
-  public static class FinishedOrders {
+  private static class FinishedOrders {
     ArrayList<Pizza> finishedOrderList = new ArrayList<>();
 
-    public void addPizza(Pizza pizza) {
+    private void addPizza(Pizza pizza) {
       finishedOrderList.add(pizza);
     }
   }
